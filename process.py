4c22aa2d30a44633f763d6b21c784fcc0895fe35
Ed Summers <ehs@pobox.com>
2019-08-01 07:19:15 -0400

    added a notebook with some network vis

--- process.py
+++ process.py
@@ -11,13 +11,11 @@ from glob import glob
 # since user ids are immutable, and screen_names can be changed.
 
 user_ids = set()
-screen_names = set()
 for line in open('seeds.jsonl'):
     user = json.loads(line)
     user_ids.add(user['id_str'])
-    screen_names.add(user['screen_name'].lower())
 
-# create a file to write matching tweets to
+# create a csv file to write matching tweets to
 
 cols = ["tweetid","userid","user_display_name","user_screen_name","user_reported_location","user_profile_description","user_profile_url","follower_count","following_count","account_creation_date","account_language","tweet_language","tweet_text","tweet_time","tweet_client_name","in_reply_to_userid","in_reply_to_tweetid","quoted_tweet_tweetid","is_retweet","retweet_userid","retweet_tweetid","latitude","longitude","quote_count","reply_count","like_count","retweet_count","hashtags","urls","user_mentions","poll_choices"]
 output = csv.DictWriter(open('results/tweets.csv', 'w'), fieldnames=cols)
@@ -25,10 +23,11 @@ output.writeheader()
 
 # go through each of the tweets csv files and write any matches to our output file
 
-count = 0
+matches = count = 0
 for csv_file in glob('data/*tweets*.csv'):
     with open(csv_file) as fh:
         for tweet in csv.DictReader(fh):
+            count += 1
             match = False
 
             # check these columns to see if our seed user_id is there
@@ -36,14 +35,15 @@ for csv_file in glob('data/*tweets*.csv'):
                 if tweet[prop] in user_ids:
                     match = True
 
-            # user_mentions has the screen_name not user_id
-            if tweet['user_mentions'].lower() in screen_names:
+            # unpack user_mentions which is a string like ["1234","5689"]
+            user_mentions = set(tweet['user_mentions'].strip('"[] ').split(','))
+            if user_mentions & user_ids :
                 match = True
 
             if match:
-                count += 1
+                matches += 1
                 output.writerow(tweet)
                 sys.stdout.write('.')
                 sys.stdout.flush()
 
-print('\n{} matches!'.format(count))
+print('\n{}/{} matches!'.format(matches, count))
